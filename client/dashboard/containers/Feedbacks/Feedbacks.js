aaacd1899f1257e5560f631f0272b6edb9da4881
Sri Majji <sri.majji@outlook.com>
2017-03-02 23:47:13 -0800

    wire feedback table with backend

--- client/dashboard/containers/Feedbacks/Feedbacks.js
+++ client/dashboard/containers/Feedbacks/Feedbacks.js
@@ -1,42 +1,60 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 
+import { feathersServices } from '../../feathers';
 import styles from './Feedbacks.scss';
 
-const Feedbacks = () => (
-	<div className={styles.Wrapper}>
-		<Table>
-			<TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-				<TableRow>
-					<TableHeaderColumn>Rating</TableHeaderColumn>
-					<TableHeaderColumn>Feedback</TableHeaderColumn>
-					<TableHeaderColumn>Email</TableHeaderColumn>
-				</TableRow>
-			</TableHeader>
-			<TableBody displayRowCheckbox={false}>
-				<TableRow>
-					<TableRowColumn>4.5</TableRowColumn>
-					<TableRowColumn>Employee was rude to me!</TableRowColumn>
-					<TableRowColumn>sri@sri.com</TableRowColumn>
-				</TableRow>
-				<TableRow>
-					<TableRowColumn>8</TableRowColumn>
-					<TableRowColumn>I thought there could be more machines dedicated to check workouts!</TableRowColumn>
-					<TableRowColumn>ante@gmail.com</TableRowColumn>
-				</TableRow>
-				<TableRow>
-					<TableRowColumn>9</TableRowColumn>
-					<TableRowColumn>Everything was great!</TableRowColumn>
-					<TableRowColumn>nik@gmail.com</TableRowColumn>
-				</TableRow>
-				<TableRow>
-					<TableRowColumn>1</TableRowColumn>
-					<TableRowColumn>Employee was super racist towards me and told me to go back to my country</TableRowColumn>
-					<TableRowColumn>douche@hotmail.com</TableRowColumn>
-				</TableRow>
-			</TableBody>
-		</Table>
-	</div>
-);
+class Feedbacks extends React.Component {
+	constructor() {
+		super();
 
-export default Feedbacks;
+		this._renderList = this._renderList.bind(this);
+	}
+
+	componentDidMount() {
+		this.props.dispatch(feathersServices.feedbacks.find());
+	}
+
+	_renderList() {
+		const { feedbacks } = this.props;
+		const view = feedbacks.data.map((feedback, key) => {
+			return (
+				<TableRow key={key}>
+					<TableRowColumn>{feedback.rating}</TableRowColumn>
+					<TableRowColumn>{feedback.description}</TableRowColumn>
+					<TableRowColumn>{feedback.email || 'none'}</TableRowColumn>
+				</TableRow>
+			);
+		});
+		return view;
+	}
+
+	render() {
+		const { feedbacks } = this.props;
+
+		return (
+			<div className={styles.Wrapper}>
+				<Table>
+					<TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+						<TableRow>
+							<TableHeaderColumn>Rating</TableHeaderColumn>
+							<TableHeaderColumn>Feedback</TableHeaderColumn>
+							<TableHeaderColumn>Email</TableHeaderColumn>
+						</TableRow>
+					</TableHeader>
+					<TableBody displayRowCheckbox={false}>
+						{feedbacks ? this._renderList() : null}
+					</TableBody>
+				</Table>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	user: state.auth.user,
+	feedbacks: state.feedbacks.queryResult,
+});
+
+export default connect(mapStateToProps)(Feedbacks);
