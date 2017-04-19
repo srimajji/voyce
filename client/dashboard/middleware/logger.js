2e3cc968e812f128211a17a17e3969d86b91eb4b
Sri Majji <sri.majji@outlook.com>
2017-04-18 23:49:14 -0700

    fix redux logger middleware

--- client/dashboard/middleware/logger.js
+++ client/dashboard/middleware/logger.js
@@ -4,7 +4,7 @@
  See http://redux.js.org/docs/advanced/Middleware.html
  */
 
-const logger = store => next => action => {
+export const logger = store => next => action => {
 	console.groupCollapsed(action.type); // eslint-disable-line no-console
 	console.info('dispatching', action); // eslint-disable-line no-console
 	const result = next(action);
@@ -13,4 +13,12 @@ const logger = store => next => action => {
 	return result;
 };
 
-export default logger;
+export const crashReporter = store => next => action => {
+	try {
+		return next(action);
+	} catch (err) {
+		console.error('Caught an exception!', err); // eslint-disable-line no-console
+		throw err;
+	}
+};
+
